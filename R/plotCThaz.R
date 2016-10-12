8fd5276e3dda74a191a4643f1197d590afd20b4a
Göran Broström <goran.brostrom@umu.se>
2016-10-12 22:09:41 +0200

    add return value

--- R/plotCThaz.R
+++ R/plotCThaz.R
@@ -1,4 +1,4 @@
-plotCThaz <- function(M = 1, m = 0, what = c("cumhaz", "haz")){
+plotCThaz <- function(M = 1, m = 0, what = c("cumhaz", "haz"), fig = TRUE){
     ## CT = Coale-Trussell model
     what <- what[1]
     N <- c(.460, .431, .395, .322, .167, .024)
@@ -7,14 +7,17 @@ plotCThaz <- function(M = 1, m = 0, what = c("cumhaz", "haz")){
     lambda <- N * M * exp(m * V)
     x <- seq(20, 50, by = 5)
     H <- cumsum(c(0, lambda)) * 5
-    par(mfrow = c(1, 2))
-    plot(x, H, type = "l", col = "blue", xlab = "age", ylab = "Cum. hazard")
-    abline(h = 0)
-    plot(x[1:2], c(lambda[1], lambda[1]), col = "blue", 
-         type = "l", xlim = c(20, 50), ylim = c(0, max(lambda)),
-         xlab = "Age", ylab = "Hazard")
-    for (i in (2:6)){
-        lines(x[i:(i+1)], c(lambda[i], lambda[i]), col = "blue")
+    if (fig){
+        par(mfrow = c(1, 2))
+        plot(x, H, type = "l", col = "blue", xlab = "age", ylab = "Cum. hazard")
+        abline(h = 0)
+        plot(x[1:2], c(lambda[1], lambda[1]), col = "blue", 
+             type = "l", xlim = c(20, 50), ylim = c(0, max(lambda)),
+            xlab = "Age", ylab = "Hazard")
+        for (i in (2:6)){
+            lines(x[i:(i+1)], c(lambda[i], lambda[i]), col = "blue")
+        }
+        abline(h = 0)
     }
-    abline(h = 0)
+    invisible(H)
 }
