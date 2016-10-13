dcc8dabbe2369f315fe93c6e6f8e3b1dc122de2e
Göran Broström <goran.brostrom@umu.se>
2016-10-13 11:07:10 +0200

    first commit

---
+++ R/kohort.R
@@ -0,0 +1,14 @@
+kohort <- function(cohort = "1821-1840", dat = kids){
+    k1 <- dat[dat$cohort == cohort,]
+    tfr <- numeric(6)
+    lab <- levels(k1$hisclass)
+    for (i in 1:6){
+        d <- k1[k1$hisclass == lab[i], ]
+        n.kids <- sum(d$event)
+        mothers <- length(unique(d$id))
+        exposure <- sum(d$exposure)
+        tfr[i] <- n.kids / mothers
+    }
+    names(tfr) <- lab
+    tfr
+}
